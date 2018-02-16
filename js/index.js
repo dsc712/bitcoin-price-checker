3699b16ea329e9a88731f6d645e702c50021f8c0
dsc712 <devyendushekhar712@gmail.com>
2018-02-16 11:17:40 +0530

    solve onload data fetch problem

--- js/index.js
+++ js/index.js
@@ -4,6 +4,20 @@ var currency1 = document.querySelector("#currency1") ;
 var currency2 = document.querySelector("#currency2") ;
 var currency3 = document.querySelector("#currency3") ;
 
+currency1.checked = true ;
+
+var xhr1 = new XMLHttpRequest() ;
+xhr1.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json") ;
+xhr1.onload = function(){
+  if(xhr1.readyState == 4 && xhr1.status == 200 )
+    {
+      var data = JSON.parse(xhr1.responseText) ; 
+      price.innerText = data.bpi.USD.rate_float + "  USD";
+    }
+}
+xhr1.send() ;
+
+
 btn.addEventListener("click" , function(){
   var xhr = new XMLHttpRequest();
   
